e3bfdf07c03ce8370f9f061b8cf0cb5c167400c4
Buttars9 <austinrbuttars@gmail.com>
2025-10-10 14:56:23 -0600

    � Multi-file HelpModal integration and helpContent expansion

--- frontend/src/components/estate/POAGenerator.jsx
+++ frontend/src/components/estate/POAGenerator.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import GlowingBox from '../GlowingBox';
 import { useWizardStore } from '../../stores/wizardStore';
